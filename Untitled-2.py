504bac653573fb0dc481cdfb5f87b4ae03526ad3
BBB <beniaminrusnac556@gmail.com>
2026-09-05 20:40:49 +0300

    Add files via upload

---
+++ Untitled-2.py
@@ -0,0 +1,76 @@
+
+
+# print(
+# """
+# ----- CV -----
+# Nume: Beniamin Rusnac
+
+# Adresă: Chișinău, Republica Moldova
+# Telefon: +373 67 233 546
+# Email: beniaminrusnac556@gmail.com
+# Studii: Step IT
+# Educație: Liceul Teoretic (absolvent)
+# Competențe: Python, HTML, CSS, Git, SQL
+# Limbi vorbite: Română, Engleză, Rusă
+# Hobby-uri: Tehnologie, Jocuri video, Fotbal
+# Obiectiv profesional: Să devin programator profesional
+# """")
+
+# ----------------
+# """
+# \n
+
+# \t tabulare adauga mai multe spatii intre text
+
+#  sep=" " schimba separarea intre textele scrise 
+
+
+
+# print("S\tT\tE\tP")
+
+# print(" \"Step\" \'2025\' \\LPD2521ro/ ")
+# print("Nume" , "Prenume" , "Grupa", sep="\n")
+# print ("Step" , end="-")
+# print ("2025" , end="-")
+#   variabile -  o portiune de memorie in care putem pastra o informatie pe parcursul scriptului 
+
+# Reguli de definiri a VAR
+# VARIABI;E
+# 1. NUme var - LItere , cifre si _
+# 2. Nume este un cuvint 
+# 3 LITERA mare si litera mica sunt litere diferite '' STEP  step ''
+# 4 informatia = valoarea  STEP = 123
+# tip date 
+# 1. integer - 1 12 123
+# a = 2025 
+# a= b = v = 2025
+# 2. float - 0.0001 1.001 123.321
+# a = b =1.1
+# 3. binare - bool - 0 1 => 1= True 0 = False
+#  a = true 
+# 4. siruri - string "valoare sir " 'valoare sir'
+# a= "STEP"d
+#  4 var - cu valori de diferite tip de date 
+# var1 = 123
+# var2 = 123.321
+# var3 = True 
+# var4 = "STEP 2025"
+# print(" var1= " ,var1 , ",type(var1) = ",type (var1), ", adresa = ", id(var1) )
+# print(" var2= " ,var2 ," ,type(var2) = ",type (var2) , ", adresa = ", id(var2))
+# print(" var3= " ,var3 ,", type(var3) = ",type (var3) , ", adresa = ", id(var3))
+# print(" var4= " ,var4 ," , type(var4) = ", type(var4) , ", adresa = ", id(var4))
+#  sarcin S se afiseze virsta daca an nasstrere si an curent
+# varsta = 15
+# an_nastere = 2010
+# an_curent = 2025
+# print("Varstta:" , varsta)
+# print("An nastrere:", an_nastere)
+# print("An curent:", an_curent) 
+# a = " 123"
+
+# R = a*2
+ 
+# math.functia_necesara
+# IF 
+
+      
